1aadea49f93096e5522eac2ab17944b5ec360b01
Vishnu <vishnuvinod072@gmail.com>
2023-09-13 14:09:57 +0530

    add conditional render

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,28 @@ import { type AppType } from "next/app";
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import { api } from "@/lib/api";
+import Navbar from "@/components/Navbar";
+import { useRouter } from "next/router";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+  const isReader = router.pathname.startsWith("/f/");
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Component {...pageProps} />
+        {isReader ? (
+          <>
+            <Component {...pageProps} />
+          </>
+        ) : (
+          <main className="mx-auto max-w-5xl px-4 lg:px-16">
+            <Navbar />
+            <Component {...pageProps} />
+          </main>
+        )}
       </ThemeProvider>
     </SessionProvider>
   );
