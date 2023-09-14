eabfb89e9fe46f5bbcc4f72ffce7006594030813
Vishnu <vishnuvinod072@gmail.com>
2023-09-14 21:14:01 +0530

    add debounce

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,19 +2,36 @@ import { BlockNoteEditor } from "@blocknote/core";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import "@blocknote/core/style.css";
 import { useTheme } from "next-themes";
+import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 function Editor() {
   const { theme } = useTheme();
+  // const [markdown, setMarkdown] = useState<string>("");
 
-  const editor: BlockNoteEditor | null = useBlockNote({});
+  const debounced = useDebouncedCallback((value) => {
+    console.log("saving to db", value);
+  }, 3000);
+
+  const editor: BlockNoteEditor | null = useBlockNote({
+    onEditorContentChange: (editor) => {
+      // setMarkdown(JSON.stringify(editor.topLevelBlocks));
+      debounced(JSON.stringify(editor.topLevelBlocks));
+    },
+  });
 
   return (
-    <BlockNoteView
-      className="h-full"
-      // add default value and onUpdate with debouncing
-      theme={theme === "dark" ? "dark" : "light"}
-      editor={editor}
-    />
+    <>
+      <BlockNoteView
+        className="h-full"
+        theme={theme === "dark" ? "dark" : "light"}
+        editor={editor}
+        // defaultValue={JSON.parse(
+        //   '[{"type":"paragraph","children":[{"text":"Hello world!"}]}]',
+        // )}
+      />
+      {/* <pre>{markdown}</pre> */}
+    </>
   );
 }
 
