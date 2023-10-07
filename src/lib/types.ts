a88686c5c0436d2031446e7dcfa157731daa1cec
Vishnu <vishnuvinod072@gmail.com>
2023-10-07 18:23:13 +0530

    add types for highlight

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,40 @@
-export enum HighlightType {
+import { Highlight } from "react-pdf-highlighter";
+
+export enum HighlightContentType {
   TEXT = "TEXT",
   IMAGE = "IMAGE",
+}
+
+export interface HighlightType {
+  content:
+    | {
+        text: string;
+      }
+    | {
+        image: string;
+      };
+  position: HighlightPositionType;
+  id: string;
+}
+
+export interface HighlightPositionType {
+  boundingRect: {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    width: number;
+    height: number;
+    pageNumber?: number;
+  };
+  rects: {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    width: number;
+    height: number;
+    pageNumber?: number;
+  }[];
+  pageNumber: number;
 }
