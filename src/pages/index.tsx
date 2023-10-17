dc036862a251dd805b3b18c0c0b7bd26575d9b0d
Vishnu <vishnuvinod072@gmail.com>
2023-10-17 13:05:43 +0530

    add a nice background to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
       <div className="flex flex-col items-center justify-center gap-2 py-8 lg:pt-16">
         <p className="text-4xl font-semibold tracking-tighter ">
           Create. Collaborate. Captivate.
