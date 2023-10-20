963396bbb614a1955d11404be1cc46a0803abf00
Vishnu <vishnuvinod072@gmail.com>
2023-10-20 22:31:22 +0530

    fix types

--- src/server/api/routers/document.ts
+++ src/server/api/routers/document.ts
@@ -24,6 +24,7 @@ export const documentRouter = createTRPCRouter({
             },
           ],
         },
+
         include: {
           highlights: {
             include: {
@@ -37,25 +38,45 @@ export const documentRouter = createTRPCRouter({
         },
       });
 
-      const highlightData = res?.highlights.map((highlight) => ({
+      if (!res) return null;
+
+      const highlightData = res.highlights.map((highlight) => ({
         ...(highlight.text
           ? { content: { text: highlight.text } }
           : { content: { image: highlight.imageUrl } }),
         id: highlight.id,
         position: {
-          boundingRect: highlight.boundingRectangle,
-          rects: highlight.rectangles,
+          boundingRect: {
+            id: highlight.boundingRectangle?.id!,
+            x1: highlight.boundingRectangle?.x1!,
+            y1: highlight.boundingRectangle?.y1!,
+            x2: highlight.boundingRectangle?.x2!,
+            y2: highlight.boundingRectangle?.y2!,
+            width: highlight.boundingRectangle?.width!,
+            height: highlight.boundingRectangle?.height!,
+            pageNumber: highlight.boundingRectangle?.pageNumber!,
+          },
+          rects: highlight.rectangles.map((rect) => ({
+            id: rect.id,
+            x1: rect.x1,
+            y1: rect.y1,
+            x2: rect.x2,
+            y2: rect.y2,
+            width: rect.width,
+            height: rect.height,
+            pageNumber: rect.pageNumber,
+          })),
           pageNumber: highlight.pageNumber,
         },
       }));
 
       return {
-        id: res?.id,
-        title: res?.title,
-        highlights: highlightData,
-        owner: res?.owner,
-        collaborators: res?.collaborators,
-        messages: res?.messages,
+        id: res.id,
+        title: res.title,
+        highlights: highlightData!,
+        owner: res.owner,
+        collaborators: res.collaborators,
+        messages: res.messages,
       };
     }),
 });
