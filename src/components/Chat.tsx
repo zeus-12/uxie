9d9930e2976192ffeee850f9e0080f2ed948336b
Vishnu <vishnuvinod072@gmail.com>
2023-10-27 00:45:42 +0530

    fix chat ui

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,8 @@
+import { api } from "@/lib/api";
 import { cn } from "@/lib/utils";
 import { useChat } from "ai/react";
 import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 
 export default function Chat() {
@@ -14,33 +16,53 @@ export default function Chat() {
     },
   });
 
-  return (
-    <div className="stretch mx-auto flex w-full flex-col gap-2 py-24">
-      {messages.map((m) => (
-        <div
-          key={m.id}
-          className={cn(
-            m.role === "user" && "ml-auto bg-blue-500 text-white",
-            m.role === "assistant" && "mr-auto bg-gray-200 text-black",
-            "max-w-[90%] select-none rounded-xl px-3 py-1 text-left ",
-          )}
-        >
-          <ReactMarkdown>{m.content}</ReactMarkdown>
-        </div>
-      ))}
+  //implement autoscrolling, and infinite loading => also fetch the messages from prev session and display
+  // const { data: messages } = api.message.getAll.useQuery({
+  //   docId: docId as string,
+  //   userId: userId as string,
+  // })
 
-      <div className="fixed bottom-4 ">
-        <form onSubmit={handleSubmit}>
-          <div className="border shadow-xl">
-            <input
-              className="w-[80%] rounded border-0 border-gray-300 p-2 "
-              value={input}
-              onChange={handleInputChange}
-            />
-            <button type="submit">Send</button>
+  const messageWindowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageWindowRef.current) {
+      messageWindowRef.current.scrollTop =
+        messageWindowRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  return (
+    <div className="flex h-full w-full flex-col gap-2 overflow-hidden">
+      <div
+        className="hideScrollbar flex flex-1 flex-col gap-2 overflow-auto"
+        ref={messageWindowRef}
+      >
+        {messages.map((m) => (
+          <div
+            key={m.id}
+            className={cn(
+              m.role === "user" && "ml-auto bg-blue-500 text-white",
+              m.role === "assistant" && "mr-auto bg-gray-200 text-black",
+              "max-w-[90%] select-none rounded-xl px-3 py-1 text-left ",
+            )}
+          >
+            <ReactMarkdown>{m.content}</ReactMarkdown>
           </div>
-        </form>
+        ))}
       </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="flex w-full rounded-md border shadow-xl">
+          <input
+            className="flex-1 rounded border-0 border-gray-300 p-2 "
+            value={input}
+            onChange={handleInputChange}
+          />
+          <button className="w-fit bg-gray-50 px-2" type="submit">
+            Send
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
