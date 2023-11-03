45cb7e8aff73d3b8e31a4e4c2fdb6b116ed43213
Vishnu <vishnuvinod072@gmail.com>
2023-11-03 12:23:40 +0530

    more homepage ui change, add features section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Features from "@/components/Features";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,10 +8,10 @@ export default function Home() {
     <div>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] dark:bg-gray-950 dark:bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] dark:[mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       <div className="flex flex-col items-center justify-center gap-2 py-8 lg:pt-16">
-        <p className="text-4xl font-semibold tracking-tighter ">
+        <p className="text-5xl font-bold tracking-tight ">
           Create. Collaborate. Captivate.
         </p>
-        <p className="text-sm text-gray-400">
+        <p className="text-lg text-gray-400">
           It&apos;s not just reading anymore, It&apos;s a conversation.
         </p>
         <Link href="/f">
@@ -24,6 +25,8 @@ export default function Home() {
         alt="Demo"
         className="rounded-sm"
       />
+
+      <Features />
     </div>
   );
 }
