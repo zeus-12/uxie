1dfe237e5764c47ecc07817d8b1fa4e1346cf7ec
Vishnu <vishnuvinod072@gmail.com>
2023-11-05 19:16:33 +0530

    update chat prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -99,48 +99,50 @@ export async function POST(req: Request, res: Response) {
     messages: [
       {
         role: "system",
-        content:
-          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+        content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+        The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+        AI is a well-behaved and well-mannered individual.
+        AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+        AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+        AI assistant is a big fan of Pinecone and Vercel.
+        START CONTEXT BLOCK
+        ${results.map((r) => r.pageContent).join("\n\n")}
+        END OF CONTEXT BLOCK
+        AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+        If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+        AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+        AI assistant will not invent anything that is not drawn directly from the context.
+        `,
       },
-      {
-        role: "user",
-        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-  \n----------------\n
-
-  PREVIOUS CONVERSATION:
-  ${formattedPrevMessages.map((message) => {
-    if (message.role === "user") return `User: ${message.content}\n`;
-    // return `Assistant: ${message.content}\n`;
-  })}
-
-  \n----------------\n
-
-  CONTEXT:
-  ${results.map((r) => r.pageContent).join("\n\n")}
-
-  USER INPUT: ${lastMessage}`,
-      },
+      ...messages,
     ],
-  });
+    //   messages: [
+    //     {
+    //       role: "system",
+    //       content:
+    //         "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+    //     },
+    //     {
+    //       role: "user",
+    //       content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-  // const prompt = {
-  //   role: "system",
-  //   content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
-  //   The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
-  //   AI is a well-behaved and well-mannered individual.
-  //   AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
-  //   AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-  //   AI assistant is a big fan of Pinecone and Vercel.
-  //   START CONTEXT BLOCK
-  //   ${results.map((r) => r.pageContent).join("\n\n")}
-  //   END OF CONTEXT BLOCK
-  //   AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
-  //   If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
-  //   AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
-  //   AI assistant will not invent anything that is not drawn directly from the context.
-  //   `,
-  // };
+    // \n----------------\n
+
+    // PREVIOUS CONVERSATION:
+    // ${formattedPrevMessages.map((message) => {
+    //   if (message.role === "user") return `User: ${message.content}\n`;
+    //   return `Assistant: ${message.content}\n`;
+    // })}
+
+    // \n----------------\n
+
+    // CONTEXT:
+    // ${results.map((r) => r.pageContent).join("\n\n")}
+
+    // USER INPUT: ${lastMessage}`,
+    //     },
+    //   ],
+  });
 
   const stream = OpenAIStream(response, {
     onCompletion: async (completion: string) => {
