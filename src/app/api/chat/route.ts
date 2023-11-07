28f25271c9e2a1cacb37385cccd476cbdfb88192
Vishnu <vishnuvinod072@gmail.com>
2023-11-07 19:41:22 +0530

    update chat endpoint prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -102,7 +102,6 @@ export async function POST(req: Request, res: Response) {
         AI is a well-behaved and well-mannered individual.
         AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
         AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
-        AI assistant is a big fan of Pinecone and Vercel.
         START CONTEXT BLOCK
         ${results.map((r) => r.pageContent).join("\n\n")}
         END OF CONTEXT BLOCK
