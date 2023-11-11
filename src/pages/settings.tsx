aa88a6d57867405285a571545498486cb16f134f
Vishnu <vishnuvinod072@gmail.com>
2023-11-11 14:21:31 +0530

    remove settings page

--- src/pages/settings.tsx
+++
@@ -1,9 +0,0 @@
-const UserSettings = () => {
-  return (
-    <div>
-      user settings page, letting users customise: 1. behaviour on adding
-      highlight : whether it should get added to the md editor as well 2.
-    </div>
-  );
-};
-export default UserSettings;
