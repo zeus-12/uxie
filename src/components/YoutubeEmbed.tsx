d0d3ed9c92df987c9d11752b2ab25c17e4831496
Vishnu <vishnuvinod072@gmail.com>
2023-11-11 14:16:09 +0530

    use react-youtube

---
+++ src/components/YoutubeEmbed.tsx
@@ -0,0 +1,30 @@
+import YouTube, { YouTubeEvent, YouTubeProps } from "react-youtube";
+
+interface YoutubeEmbedProps {}
+
+const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({}) => {
+  const onPlayerReady: YouTubeProps["onReady"] = (event: YouTubeEvent<any>) => {
+    event.target.pauseVideo();
+  };
+
+  const opts: YouTubeProps["opts"] = {
+    height: "390",
+    width: "640",
+
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+  return (
+    <div className="mb-48 flex flex-col items-center">
+      <YouTube
+        iframeClassName="rounded-md aspect-video h-96  max-w-[90vw]"
+        videoId="m97zcPWSceU"
+        opts={opts}
+        onReady={onPlayerReady}
+      />
+    </div>
+  );
+};
+
+export default YoutubeEmbed;
