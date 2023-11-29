e5fa07661881f9a9adf253fe513d5637e03ca2d2
Vishnu <vishnuvinod072@gmail.com>
2023-11-29 17:15:04 +0530

    more types

--- src/types/editor.ts
+++ src/types/editor.ts
@@ -1,6 +1,14 @@
 import { schemaWithCustomBlocks } from "@/lib/editor-utils";
 import { BlockNoteEditor } from "@blocknote/core";
+import * as Y from "yjs";
 
 export type BlockNoteEditorType = BlockNoteEditor<
   typeof schemaWithCustomBlocks
 >;
+
+export type YjsEditorProps = {
+  doc: Y.Doc;
+  provider: any;
+  canEdit: boolean;
+  username: string;
+};
