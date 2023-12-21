86613388bbfeed34e1c86cb485af7df4fab39e02
Vishnu <vishnuvinod072@gmail.com>
2023-12-21 23:00:31 +0530

    remove  unused

--- src/lib/editor-utils.ts
+++ src/lib/editor-utils.ts
@@ -1,35 +1,39 @@
 import {
-  createAlertBlock,
+  alertBlock,
   insertAlert,
 } from "@/components/Editor/CustomBlocks/Alert";
-import { createHighlightBlock } from "@/components/Editor/CustomBlocks/Highlight";
-import { Block, BlockSpec, PropSchema } from "@blocknote/core";
+import { highlightBlock } from "@/components/Editor/CustomBlocks/Highlight";
+import { getBlockSchemaFromSpecs, defaultBlockSpecs } from "@blocknote/core";
 import {
   defaultBlockTypeDropdownItems,
   getDefaultReactSlashMenuItems,
 } from "@blocknote/react";
 import { AlertCircle } from "lucide-react";
-import { defaultBlockSchema } from "@blocknote/core";
 
-export const blockTypeDropdownItems = [
-  ...defaultBlockTypeDropdownItems,
-  {
-    name: "Alert",
-    type: "alert",
-    icon: AlertCircle as any,
-    isSelected: (
-      block: Block<Record<string, BlockSpec<string, PropSchema, boolean>>>,
-    ) => block.type === "alert",
-  },
-];
+export const blockTypeDropdownItems =
+  // :BlockTypeDropdownItem[]
+  [
+    ...defaultBlockTypeDropdownItems,
+    {
+      name: "Alert",
+      type: "alert",
+      icon: AlertCircle,
 
-export const schemaWithCustomBlocks = {
-  ...defaultBlockSchema,
-  alert: createAlertBlock(),
-  highlight: createHighlightBlock(),
+      // isSelected: (
+      //   block: Block<Record<string, BlockSpec<string, PropSchema, boolean>>>,
+      // ) => block.type === "alert",
+    },
+  ];
+
+export const blockSpecs = {
+  ...defaultBlockSpecs,
+  alert: alertBlock,
+  highlight: highlightBlock,
 };
 
+export const blockSchema = getBlockSchemaFromSpecs(blockSpecs);
+
 export const slashMenuItems = [
-  ...getDefaultReactSlashMenuItems(schemaWithCustomBlocks),
+  ...getDefaultReactSlashMenuItems(blockSchema),
   insertAlert,
 ];
