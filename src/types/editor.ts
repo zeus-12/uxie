1c44a999a78475f8de4afec0d605d20725839395
Vishnu <vishnuvinod072@gmail.com>
2023-12-21 23:01:41 +0530

    rename

--- src/types/editor.ts
+++ src/types/editor.ts
@@ -1,10 +1,8 @@
-import { schemaWithCustomBlocks } from "@/lib/editor-utils";
+import { blockSchema } from "@/lib/editor-utils";
 import { BlockNoteEditor } from "@blocknote/core";
 import * as Y from "yjs";
 
-export type BlockNoteEditorType = BlockNoteEditor<
-  typeof schemaWithCustomBlocks
->;
+export type BlockNoteEditorType = BlockNoteEditor<typeof blockSchema>;
 
 export type YjsEditorProps = {
   doc: Y.Doc;
