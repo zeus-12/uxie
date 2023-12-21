8f29151001cc8118886c92f4767775e33d27adb3
Vishnu <vishnuvinod072@gmail.com>
2023-12-21 23:00:55 +0530

    remove unused, more changes

--- src/components/Editor/CustomBlocks/Alert.tsx
+++ src/components/Editor/CustomBlocks/Alert.tsx
@@ -1,168 +1,136 @@
-import {
-  BlockNoteEditor,
-  BlockSpec,
-  DefaultBlockSchema,
-  defaultProps,
-  PropSchema,
-  SpecificBlock,
-} from "@blocknote/core";
-import {
-  createReactBlockSpec,
-  InlineContent,
-  ReactSlashMenuItem,
-} from "@blocknote/react";
+import { defaultProps } from "@blocknote/core";
+import { ReactSlashMenuItem, createReactBlockSpec } from "@blocknote/react";
 import { AlertCircle, Ban, Check, Info, ShieldAlertIcon } from "lucide-react";
-import { useState } from "react";
+import { createElement } from "react";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { blockSchema } from "@/lib/editor-utils";
 
-export const alertTypes = {
+const alertTypes = {
   warning: {
     icon: ShieldAlertIcon,
     color: "#e69819",
-    backgroundColor: {
-      light: "#fff6e6",
-    },
+    backgroundColor: "#fff6e6",
   },
   error: {
     icon: Ban,
     color: "#d80d0d",
-    backgroundColor: {
-      light: "#ffe6e6",
-    },
+    backgroundColor: "#ffe6e6",
   },
   info: {
     icon: Info,
     color: "#507aff",
-    backgroundColor: {
-      light: "#e6ebff",
-    },
+    backgroundColor: "#e6ebff",
   },
   success: {
     icon: Check,
     color: "#0bc10b",
-    backgroundColor: {
-      light: "#e6ffe6",
-    },
+    backgroundColor: "#e6ffe6",
   },
 } as const;
 
-export const alertPropSchema = {
-  textAlignment: defaultProps.textAlignment,
-  textColor: defaultProps.textColor,
-  type: {
-    default: "warning" as const,
-    values: ["warning", "error", "info", "success"] as const,
-  },
-} satisfies PropSchema;
-
-export const Alert = (props: {
-  block: SpecificBlock<
-    DefaultBlockSchema & {
-      alert: BlockSpec<"alert", typeof alertPropSchema, true>;
-    },
-    "alert"
-  >;
-  editor: BlockNoteEditor<
-    DefaultBlockSchema & {
-      alert: BlockSpec<"alert", typeof alertPropSchema, true>;
-    }
-  >;
-  theme: "light";
-}) => {
-  const [type, setType] =
-    useState<(typeof alertPropSchema.type.values)[number]>("warning");
-  const Icon = alertTypes[type].icon;
-
-  return (
-    <div
-      className="flex flex-1 items-center justify-center gap-2 break-words"
-      style={{
-        ...alertStyles,
-        backgroundColor: alertTypes[type].backgroundColor[props.theme],
-      }}
-    >
-      <Select
-        onValueChange={(value) => {
-          setType(value as (typeof alertPropSchema.type.values)[number]);
-        }}
-      >
-        <SelectTrigger className="w-fit">
-          <SelectValue>
-            <div
-              style={{
-                ...alertIconWrapperStyles,
-                backgroundColor: alertTypes[type].color,
-              }}
-              contentEditable={false}
-            >
-              <Icon
-                style={{ color: alertTypes[type].backgroundColor[props.theme] }}
-                size={24}
-              />
-            </div>
-          </SelectValue>
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {Object.entries(alertTypes).map(([key, value]) => {
-              const ItemIcon = value.icon;
-
-              return (
-                <SelectItem key={key} value={key}>
-                  <div className="flex w-fit items-center gap-2 text-xs">
-                    <ItemIcon size={12} color={value.color} />
-                    {key.slice(0, 1).toUpperCase() + key.slice(1)}
-                  </div>
-                </SelectItem>
-              );
-            })}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-
-      <InlineContent
-        style={inlineContentStyles}
-        className="break-word-overflow"
-      />
-    </div>
-  );
-};
-
-export const createAlertBlock = () =>
-  createReactBlockSpec<
-    "alert",
-    typeof alertPropSchema,
-    true,
-    DefaultBlockSchema & {
-      alert: BlockSpec<"alert", typeof alertPropSchema, true>;
-    }
-  >({
-    type: "alert" as const,
+export const alertBlock = createReactBlockSpec(
+  {
+    type: "alert",
     propSchema: {
       textAlignment: defaultProps.textAlignment,
       textColor: defaultProps.textColor,
       type: {
-        default: "warning",
-        values: ["warning", "error", "info", "success"],
+        default: "warning" as const,
+        values: ["warning", "error", "info", "success"] as const,
       },
-    } as const,
-    containsInlineContent: true,
-    render: (props) => <Alert {...props} theme={"light"} />,
-  });
+    },
+    content: "inline",
+  },
+  {
+    render: (props) => (
+      <div
+        className="flex flex-1 items-center gap-2 break-words rounded-sm p-1"
+        style={{
+          backgroundColor: alertTypes[props.block.props.type].backgroundColor,
+        }}
+      >
+        <Select
+          onValueChange={(value) => {
+            console.log(props.block.props.type);
+            props.block.props.type = value as
+              | "warning"
+              | "error"
+              | "info"
+              | "success";
+          }}
+          // value={props.block.props.type}
+        >
+          <SelectTrigger className="w-fit">
+            <SelectValue>
+              <div
+                style={{
+                  ...alertIconWrapperStyles,
+                  backgroundColor: alertTypes[props.block.props.type].color,
+                }}
+                contentEditable={false}
+              >
+                {createElement(
+                  alertTypes[props.block.props.type].icon,
+                  {
+                    style: {
+                      color: alertTypes[props.block.props.type].backgroundColor,
+                    },
+                    size: 24,
+                  },
+                  null,
+                )}
+              </div>
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {Object.entries(alertTypes).map(([key, value]) => {
+                const ItemIcon = value.icon;
 
-export const insertAlert = {
+                return (
+                  <SelectItem key={key} value={key}>
+                    <div className="flex w-fit items-center gap-2 text-xs">
+                      <ItemIcon size={12} color={value.color} />
+                      {key.slice(0, 1).toUpperCase() + key.slice(1)}
+                    </div>
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <div className={"inline-content"} ref={props.contentRef} />
+      </div>
+    ),
+  },
+);
+
+const alertIconWrapperStyles = {
+  borderRadius: "16px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  marginLeft: "12px",
+  marginRight: "12px",
+  height: "18px",
+  width: "18px",
+  userSelect: "none",
+  cursor: "pointer",
+} as const;
+
+export const insertAlert: ReactSlashMenuItem<typeof blockSchema> = {
   name: "Alert",
   execute: (editor) => {
     const block = editor.getTextCursorPosition().block;
-    const blockIsEmpty = block.content?.length === 0;
+    const blockIsEmpty = (block.content as any[])?.length === 0;
 
     if (blockIsEmpty) {
       editor.updateBlock(block, { type: "alert" });
@@ -192,30 +160,4 @@ export const insertAlert = {
   group: "Other",
   icon: <AlertCircle />,
   hint: "Used to emphasize text",
-} satisfies ReactSlashMenuItem<
-  DefaultBlockSchema & {
-    alert: BlockSpec<"alert", typeof alertPropSchema, true>;
-  }
->;
-
-const alertStyles = {
-  borderRadius: "4px",
-  padding: "4px",
-} as const;
-
-const alertIconWrapperStyles = {
-  borderRadius: "16px",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  marginLeft: "12px",
-  marginRight: "12px",
-  height: "18px",
-  width: "18px",
-  userSelect: "none",
-  cursor: "pointer",
-} as const;
-
-const inlineContentStyles = {
-  flexGrow: "1",
 };
