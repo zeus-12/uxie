97001dee6021777dfd1b7721db41789ab275e28c
Vishnu <vishnuvinod072@gmail.com>
2023-12-28 18:27:37 +0530

    add sm screensize

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,15 @@ export default withUt({
     "./src/**/*.{ts,tsx}",
   ],
   theme: {
+    screens: {
+      xs: "450px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+
     container: {
       center: true,
       padding: "2rem",
