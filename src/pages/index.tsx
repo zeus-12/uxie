a057834ca0f255b3caceee2828ebea44fd4f80d2
Vishnu <vishnuvinod072@gmail.com>
2024-01-25 22:11:55 +0530

    slight bg changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <div className="absolute top-0 -z-10 h-full w-full bg-white">
-        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]" />
+        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-60 blur-[80px]" />
       </div>
       <div className="px-4 py-2 lg:px-16">
         <SlideUpWhenVisible>
