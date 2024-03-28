3f3c763386d0d8a782c131be8b0af87fa4d397de
Vishnu <vishnuvinod072@gmail.com>
2024-03-28 18:05:43 +0530

    use dummy stream for dev

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,39 +1,62 @@
 import { OpenAIStream, StreamingTextResponse } from "ai";
 import fireworks from "@/lib/fireworks";
+import { env } from "@/env.mjs";
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { prompt } = await req.json();
-
-  const response = await fireworks.chat.completions.create({
-    model:
-      // "accounts/fireworks/models/llama-v2-70b-chat"
-      "accounts/fireworks/models/mixtral-8x7b-instruct",
-    messages: [
+  if (env.NODE_ENV === "development") {
+    return new StreamingTextResponse(
       {
-        role: "system",
-        content:
-          "You are an AI writing assistant that continues existing text based on context from prior text. " +
-          "Give more weight/priority to the later characters than the beginning ones. " +
-          "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
-          "Only return the text that you generate, not the prompt, only reply with the ",
+        // @ts-ignore
+        async *[Symbol.asyncIterator]() {
+          let i = 0;
+          while (i < 15) {
+            yield `${i}`;
+            i++;
+            await new Promise((resolve) => setTimeout(resolve, 100));
+          }
+          yield `done, time: ${new Date().toISOString()}`;
+        },
       },
       {
-        role: "user",
-        content: prompt,
+        headers: {
+          "Content-Type": "text/plain",
+        },
       },
-    ],
-    temperature: 0.7,
-    max_tokens: 400,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stream: true,
-    n: 1,
-  });
+    );
+  } else {
+    const { prompt } = await req.json();
 
-  const stream = OpenAIStream(response);
+    const response = await fireworks.chat.completions.create({
+      model:
+        // "accounts/fireworks/models/llama-v2-70b-chat"
+        "accounts/fireworks/models/mixtral-8x7b-instruct",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an AI writing assistant that continues existing text based on context from prior text. " +
+            "Give more weight/priority to the later characters than the beginning ones. " +
+            "Limit your response to no more than 200 characters, but make sure to construct complete sentences." +
+            "Only return the text that you generate, not the prompt, only reply with the ",
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_tokens: 400,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stream: true,
+      n: 1,
+    });
 
-  return new StreamingTextResponse(stream);
+    const stream = OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
+  }
 }
