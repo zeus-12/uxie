c8be4d1975053a4349c45355b80cdee0f096655a
Vishnu <vishnuvinod072@gmail.com>
2024-03-28 19:22:46 +0530

    update aipopover

--- src/components/Editor/AiPopover.tsx
+++ src/components/Editor/AiPopover.tsx
@@ -13,18 +13,27 @@ import ReactMarkdown from "react-markdown";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useElementSize } from "@/hooks/useElementSize";
+import BouncingDotsLoader from "@/components/BouncingDotsLoader";
+import { useBlockNoteEditor } from "@blocknote/react";
 
-const AiPopover = ({
-  rect,
-  setRect,
-}: {
-  rect: null | any;
-  setRect: (value: any) => void;
-}) => {
+type AiPopoverProps = {
+  rect: AiPopoverPropsRect;
+  setRect: (value: AiPopoverPropsRect) => void;
+};
+
+export type AiPopoverPropsRect = {
+  top: number;
+  left: number;
+  width: number;
+  blockId: string;
+} | null;
+
+const AiPopover = ({ rect, setRect }: AiPopoverProps) => {
   const [completions, setCompletions] = useState<string[]>([]);
   const [curIndex, setCurIndex] = useState<null | number>(null);
+  const editor = useBlockNoteEditor();
 
-  const { complete, completion, isLoading, stop } = useCompletion({
+  const { complete, completion, stop, isLoading } = useCompletion({
     onFinish: (_prompt, completion) => {
       setCompletions((prev) => [...prev, completion]);
     },
@@ -38,6 +47,11 @@ const AiPopover = ({
     },
   });
 
+  const closePopover = () => {
+    setRect(null);
+  };
+
+  // const isLoading = true;
   const { ref, height: aiTextBoxHeight } = useElementSize();
 
   const responseExists =
@@ -49,12 +63,51 @@ const AiPopover = ({
     setCurIndex(completions.length);
   };
 
+  if (!rect) return;
+
+  const AI_OPTIONS_AFTER_COMPLETION = [
+    {
+      title: "Replace selection",
+
+      onClick: () => {
+        console.log("clicked", curIndex);
+        if (curIndex === null) return;
+        const text = completions[curIndex];
+        console.log(text);
+        editor.updateBlock(rect.blockId, {
+          content: text,
+        });
+        closePopover();
+      },
+    },
+    {
+      title: "Insert below",
+      onClick: () => {
+        if (curIndex === null) return;
+        const text = completions[curIndex];
+        editor.insertBlocks(
+          [
+            {
+              content: text,
+              type: "paragraph",
+            },
+          ],
+          {
+            id: rect.blockId,
+          },
+          "after",
+        );
+        closePopover();
+      },
+    },
+  ];
+
   return (
     <Popover
       open={!!rect}
       onOpenChange={(open) => {
         if (!open) {
-          setRect(null);
+          closePopover();
         }
       }}
     >
@@ -64,18 +117,33 @@ const AiPopover = ({
           left: rect.left,
           width: rect.width,
         }}
-        className="absolute z-[1000] border-gray-300 bg-white p-0 text-black "
+        className="absolute z-[1000] border-gray-300 bg-white p-0 text-black"
       >
         <div ref={ref}>
-          <ReactMarkdown>
-            {responseExists ? completions[curIndex] : completion}
-          </ReactMarkdown>
+          {(isLoading || responseExists) && (
+            <ReactMarkdown className="px-2 py-1">
+              {responseExists ? completions[curIndex] : completion}
+            </ReactMarkdown>
+          )}
           {isLoading ? (
-            <div className="flex justify-between">
-              <p>AI is writing...</p>
-              <div>
-                <p>Try again</p>
-                <p onClick={stop}>Stop</p>
+            <div className="flex items-center justify-between px-2 py-1">
+              <div className="flex items-center gap-1">
+                <p>AI is writing</p>
+                <div className="scale-75">
+                  <BouncingDotsLoader />
+                </div>
+              </div>
+              <div className="flex gap-2">
+                {/* <Button variant="ghost" className="p-1 hover:cursor-pointer">
+                  <p>Try again</p>
+                </Button> */}
+                <Button
+                  variant="ghost"
+                  onClick={stop}
+                  className="p-1 hover:cursor-pointer"
+                >
+                  <p>Stop</p>
+                </Button>
               </div>
             </div>
           ) : (
@@ -137,28 +205,8 @@ const AiPopover = ({
                 minWidth: "20rem",
               }}
             >
-              {!responseExists
-                ? AI_OPTIONS_INITIAL.filter(
-                    (item) => item.items.length > 0,
-                  ).map((item) => (
-                    <div key={item.category}>
-                      <DropdownMenuLabel key={item.category}>
-                        {item.category}
-                      </DropdownMenuLabel>
-                      {item.items.map((item) => (
-                        <DropdownMenuItem
-                          onClick={() => {
-                            incrementCur();
-                            complete(item.title);
-                          }}
-                          key={item.title}
-                        >
-                          {item.title}
-                        </DropdownMenuItem>
-                      ))}
-                    </div>
-                  ))
-                : AI_OPTIONS_WITH_RESPONSE.map((item) => (
+              {responseExists
+                ? AI_OPTIONS_AFTER_COMPLETION.map((item) => (
                     <DropdownMenuItem
                       onClick={() => {
                         item.onClick();
@@ -167,7 +215,27 @@ const AiPopover = ({
                     >
                       {item.title}
                     </DropdownMenuItem>
-                  ))}
+                  ))
+                : AI_COMPLETIONS.filter((item) => item.items.length > 0).map(
+                    (item) => (
+                      <div key={item.category}>
+                        <DropdownMenuLabel key={item.category}>
+                          {item.category}
+                        </DropdownMenuLabel>
+                        {item.items.map((item) => (
+                          <DropdownMenuItem
+                            onClick={() => {
+                              incrementCur();
+                              complete(item.title);
+                            }}
+                            key={item.title}
+                          >
+                            {item.title}
+                          </DropdownMenuItem>
+                        ))}
+                      </div>
+                    ),
+                  )}
             </DropdownMenuContent>
           </DropdownMenu>
         )}
@@ -178,7 +246,7 @@ const AiPopover = ({
 
 export default AiPopover;
 
-const AI_OPTIONS_INITIAL = [
+const AI_COMPLETIONS = [
   {
     category: "Edit or review selection",
     items: [
@@ -198,17 +266,5 @@ const AI_OPTIONS_INITIAL = [
         title: "Find action items",
       },
     ],
-  },
-];
-
-const AI_OPTIONS_WITH_RESPONSE = [
-  {
-    title: "Replace selection",
-
-    onClick: () => {},
-  },
-  {
-    title: "Insert below",
-    onClick: () => {},
   },
 ];
