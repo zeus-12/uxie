6b958ff9c29de1c6cd970de0ea5cec90b1346358
Vishnu <vishnuvinod072@gmail.com>
2024-05-25 15:31:59 +0530

    add diff plans

---
+++ src/lib/constants.ts
@@ -0,0 +1,29 @@
+import { Plan } from "@prisma/client";
+
+interface PlanData {
+  title: string;
+  price: number;
+  maxDocs: number;
+  maxPagesPerDoc: number;
+  maxFileSizePerDoc: string;
+  maxCollaboratorsPerDoc: number;
+}
+
+export const PLANS: Record<Plan, PlanData> = {
+  FREE: {
+    title: "Free",
+    price: 0,
+    maxDocs: 1,
+    maxPagesPerDoc: 5,
+    maxFileSizePerDoc: "8MB",
+    maxCollaboratorsPerDoc: 0,
+  },
+  PRO: {
+    title: "Pro",
+    price: 9.99,
+    maxDocs: 100,
+    maxPagesPerDoc: 20,
+    maxFileSizePerDoc: "64MB",
+    maxCollaboratorsPerDoc: 5,
+  },
+};
