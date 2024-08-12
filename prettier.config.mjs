ba88dc03385022f0377a2423cd3a3d412d52a95d
Vishnu <vishnuvinod072@gmail.com>
2024-08-12 15:46:54 +0530

    add prettierconfig

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"],
+  bracketSpacing: true,
+  semi: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
 };
 
 export default config;
