525265ac74c997cf95300ef1c9cd2d2a7025f764
Vishnu <vishnuvinod072@gmail.com>
2024-08-30 03:33:57 +0530

    rm animations, more ui changes

--- src/components/pdf-reader/bottom-toolbar.tsx
+++ src/components/pdf-reader/bottom-toolbar.tsx
@@ -1,8 +1,7 @@
 // CREDITS: https://motion-primitives.com/docs/toolbar-dynamic
 
 import { Button } from "@/components/ui/button";
-import useClickOutside from "@/hooks/use-click-outside";
-import { MotionConfig, motion } from "framer-motion";
+import { MotionConfig } from "framer-motion";
 import { ArrowLeft, AudioLines } from "lucide-react";
 import { ReactNode, useRef, useState } from "react";
 
@@ -15,58 +14,67 @@ const transition = {
 const ReaderBottomToolbar = ({
   children,
   isAudioDisabled,
+  currentWord,
 }: {
   children: ReactNode;
   isAudioDisabled: boolean;
+  currentWord: string | undefined;
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useClickOutside(containerRef, () => {
-    setIsOpen(false);
-  });
-
   return (
     <MotionConfig transition={transition}>
       <div
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-50"
+        className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-50"
         ref={containerRef}
       >
-        <div className="h-full w-full rounded-xl border border-zinc-950/10 bg-white">
-          <motion.div
-            animate={{
-              // @todo: here I want to remove the width
-              minWidth: isOpen ? "240px" : "64px",
-            }}
+        <div className="relative flex items-stretch gap-2">
+          <div className="h-full w-full rounded-xl border border-zinc-950/10 bg-white">
+            {/* <motion.div
+            animate={
+              {
+                // @todo: here I want to remove the width
+                // minWidth: isOpen ? "200px" : "30px",
+              }
+            }
             initial={false}
-          >
-            <div className="overflow-hidden p-2">
+          > */}
+            <div className="overflow-hidden py-1 px-2">
               {!isOpen ? (
                 <div className="flex space-x-2 justify-evenly">
-                  {/* <Button disabled>
-                    <User className="h-5 w-5" />
-                  </Button> */}
                   <Button
                     onClick={() => setIsOpen(true)}
                     variant="ghost"
+                    size="sm"
                     disabled={isAudioDisabled}
                   >
                     <AudioLines className="h-5 w-5" />
                   </Button>
                 </div>
               ) : (
-                <div className="flex space-x-2">
-                  <Button variant="outline" onClick={() => setIsOpen(false)}>
+                <div className="flex space-x-2 w-fit items-center">
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    onClick={() => setIsOpen(false)}
+                  >
                     <ArrowLeft className="h-5 w-5" />
                   </Button>
-                  <div className="relative w-full">
-                    {children}
-                    <div className="absolute right-1 top-0 flex h-full items-center justify-center"></div>
-                  </div>
+                  <div className="relative w-full">{children}</div>
                 </div>
               )}
             </div>
-          </motion.div>
+            {/* </motion.div> */}
+          </div>
+
+          {isOpen && currentWord && (
+            <div className="absolute rounded-xl border border-zinc-950/10 bg-white w-fit mx-auto px-2 py-[10px] -right-1 translate-x-[100%]">
+              <p className="text-lg font-semibold text-blue-600">
+                {currentWord}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </MotionConfig>
