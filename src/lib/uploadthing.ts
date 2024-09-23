44365b82c1c69fa813e843f4a90cc2fa4bdfbd75
Vishnu <vishnuvinod072@gmail.com>
2024-09-23 21:31:13 +0530

    fmt, update env

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,5 +1,4 @@
-import { generateReactHelpers } from "@uploadthing/react";
-
 import type { OurFileRouter } from "@/server/uploadthing";
+import { generateReactHelpers } from "@uploadthing/react";
 
 export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
