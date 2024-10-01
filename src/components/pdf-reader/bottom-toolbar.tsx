7ca4a0b24b93327931040db2b17d9f479857b4a6
Vishnu <vishnuvinod072@gmail.com>
2024-10-01 22:14:51 +0530

    add accessibility props

--- src/components/pdf-reader/bottom-toolbar.tsx
+++ src/components/pdf-reader/bottom-toolbar.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import Sidebar from "@/components/workspace/sidebar";
 import { MotionConfig } from "framer-motion";
 import { ArrowLeft, AudioLines, PanelBottomOpen } from "lucide-react";
@@ -126,6 +132,10 @@ const SidebarDrawer = ({
         </Button>
       </DrawerTrigger>
       <DrawerContent className="h-5/6 mx-auto w-full bg-gray-50">
+        <DrawerTitle className="hidden">Sidebar</DrawerTitle>
+        <DrawerDescription className="hidden">
+          Sidebar with chat, notes, flashcards
+        </DrawerDescription>
         <div className="overflow-auto h-full">
           <Sidebar
             canEdit={canEdit}
