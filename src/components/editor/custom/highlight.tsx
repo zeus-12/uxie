6e284ccbf458554a1680e9fda229c0ba0cb3acff
Vishnu <vishnuvinod072@gmail.com>
2024-10-15 03:53:07 +0530

    fix highlight-block ui

--- src/components/editor/custom/highlight.tsx
+++ src/components/editor/custom/highlight.tsx
@@ -23,18 +23,26 @@ export const HighlighBlock = createReactBlockSpec(
   },
   {
     render: (props) => (
-      <div className="flex h-full max-w-full flex-1 items-center gap-2 rounded-sm p-1">
+      <div className="flex h-full w-full items-stretch gap-2 rounded-sm p-1">
         <div
           onClick={() => {
             if (!props?.block?.props?.highlightId) return;
             document.location.hash = props.block.props.highlightId;
           }}
-          className="h-full w-2 rounded-full bg-yellow-400 hover:cursor-pointer"
+          className="w-2 rounded-full bg-yellow-400 hover:cursor-pointer"
         />
         <div className="flex-1">
           <div className="inline-content" ref={props.contentRef} />
         </div>
       </div>
     ),
+    toExternalHTML: (props) => {
+      const highlightText =
+        props.block.content?.[0] && "text" in props.block.content?.[0]
+          ? props.block.content?.[0]?.text
+          : "";
+      // todo: add styles to maake it look like a highlight
+      return <p>{highlightText}</p>;
+    },
   },
 );
