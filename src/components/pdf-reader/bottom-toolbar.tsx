e8ebfe1e296417f96754ce51a29bb476a8a1f5fb
Vishnu <vishnuvinod072@gmail.com>
2024-10-14 21:35:22 +0530

    move sidebar-drawer to diff comp

--- src/components/pdf-reader/bottom-toolbar.tsx
+++ src/components/pdf-reader/bottom-toolbar.tsx
@@ -1,14 +1,7 @@
 import { Button } from "@/components/ui/button";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import Sidebar from "@/components/workspace/sidebar";
+import SidebarDrawer from "@/components/workspace/sidebar-drawer";
 import { MotionConfig } from "framer-motion";
-import { ArrowLeft, AudioLines, PanelBottomOpen } from "lucide-react";
+import { ArrowLeft, AudioLines } from "lucide-react";
 import { ReactNode, useRef, useState } from "react";
 
 const transition = {
@@ -112,40 +105,3 @@ const ReaderBottomToolbar = ({
 };
 
 export default ReaderBottomToolbar;
-
-const SidebarDrawer = ({
-  canEdit,
-  isOwner,
-  isVectorised,
-  setIsOpen,
-  isOpen,
-}: {
-  canEdit: boolean;
-  isOwner: boolean;
-  isVectorised: boolean;
-  setIsOpen: (o: boolean) => void;
-  isOpen: boolean;
-}) => {
-  return (
-    <Drawer open={isOpen} onOpenChange={(o) => setIsOpen(o)}>
-      <DrawerTrigger asChild>
-        <Button variant="ghost" size="sm">
-          <PanelBottomOpen className="h-5 w-5" />
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent className="h-5/6 mx-auto w-full bg-gray-50">
-        <DrawerTitle className="hidden">Sidebar</DrawerTitle>
-        <DrawerDescription className="hidden">
-          Sidebar with chat, notes, flashcards
-        </DrawerDescription>
-        <div className="overflow-auto h-full">
-          <Sidebar
-            canEdit={canEdit}
-            isOwner={isOwner}
-            isVectorised={isVectorised}
-          />
-        </div>
-      </DrawerContent>
-    </Drawer>
-  );
-};
---
+++ src/components/workspace/sidebar-drawer.tsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import Sidebar from "@/components/workspace/sidebar";
+import { PanelBottomOpen } from "lucide-react";
+
+const SidebarDrawer = ({
+  canEdit,
+  isOwner,
+  isVectorised,
+  setIsOpen,
+  isOpen,
+}: {
+  canEdit: boolean;
+  isOwner: boolean;
+  isVectorised: boolean;
+  setIsOpen: (o: boolean) => void;
+  isOpen: boolean;
+}) => {
+  return (
+    <Drawer open={isOpen} onOpenChange={(o) => setIsOpen(o)}>
+      <DrawerTrigger asChild>
+        <Button variant="ghost" size="sm">
+          <PanelBottomOpen className="h-5 w-5" />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent className="h-5/6 mx-auto w-full bg-gray-50">
+        <DrawerTitle className="hidden">Sidebar</DrawerTitle>
+        <DrawerDescription className="hidden">
+          Sidebar with chat, notes, flashcards
+        </DrawerDescription>
+        <div className="overflow-auto h-full">
+          <Sidebar
+            canEdit={canEdit}
+            isOwner={isOwner}
+            isVectorised={isVectorised}
+          />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default SidebarDrawer;
