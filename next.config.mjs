2033f3a8d95f2592eafd5b6040c1c5d22b1b40ed
vishnu <vishnuvinod072@gmail.com>
2026-01-30 01:43:54 +0530

    fix please?

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,59 @@
-// @ts-expect-error
-import { withGlobalCss } from "next-global-css";
+import TerserPlugin from "terser-webpack-plugin";
 
 await import("./src/env.mjs");
-
-const withConfig = withGlobalCss();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    esmExternals: "loose",
+  },
   images: {
     domains: ["lh3.googleusercontent.com", "utfs.io"],
   },
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        {
+          key: "Cross-Origin-Embedder-Policy",
+          value: "require-corp",
+        },
+        {
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
+        },
+      ],
+    },
+  ],
+
   webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
+      // for "scribe.js-ocr" library (DO NOT REMOVE)
       config.plugins.push(
         new webpack.DefinePlugin({
           process: "undefined",
         }),
       );
+
+      // for "kokoro-js" library (DO NOT REMOVE) -> issue w onnxruntime
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          exclude: /ort\..*\.mjs$/,
+          terserOptions: {
+            compress: true,
+            mangle: true,
+          },
+        }),
+      ];
     }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
 
     config.ignoreWarnings = [
       ...(config.ignoreWarnings || []),
@@ -33,4 +68,3 @@ const config = {
 };
 
 export default config;
-// export default withConfig(config);
